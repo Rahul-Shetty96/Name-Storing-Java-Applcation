017bc1f83411a0ee0f09787e8a97643bd0af2d1b
drashantpombale <prashant.dombale@gmail.com>
2017-02-13 23:12:15 +0530

    Update ViewFrame.java

--- ViewFrame.java
+++ ViewFrame.java
@@ -6,5 +6,35 @@ import java.sql.*;
 
 
 public class ViewFrame extends JFrame{
+JTextArea t;
+JScrollPane sp;
+
+public ViewFrame(){
+super("View All Employees");
+setSize(500,200);
+setResizable(false);
+
+t=new JTextArea(10,10);
+t.setEditable(false);
+
+sp=new JScrollPane(t);
+sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+add(sp);
+
+//Database command
+
+setLocationRelativeTo(null);
+setVisible(true);
+
+addWindowListener(new WindowAdapter(){
+public void windowClosing(WindowEvent we){
+HomeFrame h = new HomeFrame();
+dispose();
+}
+});
+}
+
 }
 	
