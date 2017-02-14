6cdd16a11f725ac02b0a4c1a21cbc523db003bd6
RAHUL SHETTY <rahul.shetty@ves.ac.in>
2017-02-15 02:07:36 +0530

    Sound added,DeleteFrame amd ModifyFrame added

--- ViewFrame.java
+++ ViewFrame.java
@@ -3,38 +3,40 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.sql.*;
+import app.*;
 
 
 public class ViewFrame extends JFrame{
 JTextArea t;
 JScrollPane sp;
 
-public ViewFrame(){
-super("View All Employees");
-setSize(500,200);
-setResizable(false);
+		public ViewFrame(){
+				super("View All Employees");
+				setSize(500,200);
+				setResizable(false);
 
-t=new JTextArea(10,10);
-t.setEditable(false);
+				t=new JTextArea(10,10);
+				t.setEditable(false);
 
-sp=new JScrollPane(t);
-sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+				sp=new JScrollPane(t);
+				sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+				sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
-add(sp);
+				add(sp);
 
-//Database command
+				DatabaseHandler db = new DatabaseHandler();
+				t.setText(db.query());
 
-setLocationRelativeTo(null);
-setVisible(true);
+				setLocationRelativeTo(null);
+				setVisible(true);
 
-addWindowListener(new WindowAdapter(){
-public void windowClosing(WindowEvent we){
-HomeFrame h = new HomeFrame();
-dispose();
+				addWindowListener(new WindowAdapter(){
+				public void windowClosing(WindowEvent we){
+				HomeFrame h = new HomeFrame();
+				dispose();
+				}
+				});
+		}
+
 }
-});
-}
-
-}
-	
+			
